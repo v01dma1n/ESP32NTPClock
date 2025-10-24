1927b1f4f2f2a7a6f35eb0180aecd845596fbac2
v01dma1n <irek@rybark.com>
2025-10-24 14:15:29 -0400

    live update fixes

--- src/display_manager.cpp
+++ src/display_manager.cpp
@@ -17,12 +17,28 @@ void DisplayManager::setAnimation(std::unique_ptr<IAnimation> animation) {
 }
 
 void DisplayManager::update() {
+    bool animationFinished = false;
+
     if (_currentAnimation) {
         _currentAnimation->update();
         if (_currentAnimation->isDone()) {
             _currentAnimation.reset();
+            animationFinished = true; // Set the flag
         }
     }
+
+    // If an animation just finished OR if no animation is running,
+    // we still need to tell the driver to push its current buffer state
+    // to the physical display hardware.
+    // For drivers that need continuous updates (like VFDs handled by a task),
+    // this call might do nothing, which is fine.
+    _display.writeDisplay();
+
+    // If an animation just finished, clear the display *after* writing the last frame
+    // if (animationFinished) {
+    //     _display.clear();
+    //     _display.writeDisplay(); // Write the cleared state
+    // }
 }
 
 bool DisplayManager::isAnimationRunning() const {
